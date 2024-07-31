93c0a68020936b63e2b69f6d2908b7eb5b855372
MounikaKarna <karnamounika72@gmail.com>
2024-07-31 18:52:19 +0530

    updated settings and SW versions

--- LumaProject/src/main/java/com/luma/pages/HoodiesPage.java
+++ LumaProject/src/main/java/com/luma/pages/HoodiesPage.java
@@ -9,7 +9,7 @@ import com.luma.base.TestBase;
 public class HoodiesPage extends TestBase{
 	@FindBy(xpath = "//span[contains(text(),'Sale')]") WebElement Sale;
 	@FindBy(xpath = "(//a[text()='Hoodies and Sweatshirts'])[1]") WebElement hoodiesbtn;
-	@FindBy(xpath = "(//img[@alt='Autumn Pullie'])[2]") WebElement Image;
+	@FindBy(xpath = "//img[@alt='Autumn Pullie']") WebElement Image;
 	@FindBy(xpath = "//div[text()='S']") WebElement Size;
 	@FindBy(xpath = "//div[@option-label='Red']") WebElement Colour;
 	@FindBy(xpath = "//button[@title='Add to Cart']") WebElement AddToCart;
